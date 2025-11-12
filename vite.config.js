7872a566b50f5994c89895fefd331216c610cc6d
TiusPutra <Tiusid301@gmail.com>
2025-11-12 11:38:47 +0800

    Rollback

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/repository-name/', // ganti dengan nama repo kamu
-  plugins: [react()]
+  plugins: [react()],
 })
